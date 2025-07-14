83db2b03cef5cd7966a94aff2a34128bf2ad7e23
Nehaal <nehaalraj10@gmail.com>
2025-07-14 10:34:28 +0530

    sdzc

--- apsera_sim/ENOB.py
+++
@@ -1,5 +0,0 @@
-def ENOB(SINAD_value):
-
-    ENOB = (SINAD_value - 1.76)/6.02
-
-    return ENOB
--- apsera_sim/sine_input.py
+++ apsera_sim/sine_input.py
@@ -4,8 +4,7 @@ def sine_curve(f,sampling_rate,duration,phase,acc):
     n_samples = int(duration*sampling_rate)
     t = np.linspace(0+acc*duration,duration+acc*duration,n_samples, endpoint= False)
     phase_rad = np.deg2rad(phase)
-    scaling_1dbm = 0.3546 # use for all other files but char_fp and hlaf of Vref for its scaling factor in accumulation.py
-    scaling_3dbm = 0.447
+    scaling_1dbm = 0.3546 
     sine_sig = scaling_1dbm*((np.sin(2*np.pi*(f)*t+phase_rad))) + scaling_1dbm
     # offset and scaling to fit adc range 
     
